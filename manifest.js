0ba06de2763820d9c778f3b50c7d5028665b69c2
Git-Galeon <fgalaz1994.au@gmail.com>
2026-06-03 10:52:10 -0400

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "Neto Control Financiero",
+  "short_name": "Neto",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#0f172a",
+  "theme-color": "#1e293b",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/2489/2489756.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
