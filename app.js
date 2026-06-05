d8efb0af0dc43c49bb7e7df7a0e9f70abacd733a
Git-Galeon <fgalaz1994.au@gmail.com>
2026-06-05 09:05:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz5Gp8Xwe9lwtjFHOazMYucOrRvaM64hPfcGsSFNzEVUdviAeRJTt3UIPTVKREySl8o/exec"; // Reemplaza con tu URL de GAS
+const API_URL = "https://script.google.com/macros/s/AKfycbyIC3yGswFCICi79jewstcjRmyluREb22mtBxVUlJuyR8CPTc23KSdK36NXaEDYRFuv/exec"; // Reemplaza con tu URL de GAS
 const TOKEN = "A_gam3_n0n-7&7";
 
 // Iniciar PWA Service Worker
